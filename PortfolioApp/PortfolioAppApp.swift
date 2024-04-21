e1a4e280b77beaef24b601e188fed9c6427e2368
Trần Việt Anh <tranvietanh1803@gmail.com>
2024-04-21 09:02:47 +0700

    Initial Commit

---
+++ PortfolioApp/PortfolioAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PortfolioAppApp.swift
+//  PortfolioApp
+//
+//  Created by Tran Viet Anh on 21/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct PortfolioAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
